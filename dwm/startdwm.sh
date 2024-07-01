e46fd11730ef3f5bfaeb794096544e2c50fe765b
TolaMironcenko <mironcenkotola@gmail.com>
2024-07-01 09:11:12 +0700

    minimal dwm added

---
+++ dwm/startdwm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+setxkbmap "us,ru" -option 'grp:win_space_toggle'
+feh --bg-fill ~/Pictures/hyprpaper/images/beautiful-nature-landscape-with-mountains-lake.png
+picom -b
+
+while true; do
+    dwm 2> ~/.dwm.log
+done
+
