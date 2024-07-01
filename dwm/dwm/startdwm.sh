4b14e7f00bc4e2f9108d8c7d8abfb8c496c16515
TolaMironcenko <mironcenkotola@gmail.com>
2024-07-01 17:29:08 +0700

    dwm and dwmbar

--- dwm/dwm/startdwm.sh
+++ dwm/dwm/startdwm.sh
@@ -3,6 +3,7 @@
 setxkbmap "us,ru" -option 'grp:win_space_toggle'
 feh --bg-fill ~/Pictures/hyprpaper/images/beautiful-nature-landscape-with-mountains-lake.png
 picom -b
+~/dotfiles/dwm/dwmbar/dwmbar &
 
 while true; do
     dwm 2> ~/.dwm.log
