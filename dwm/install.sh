e1821928c709c8f30f495492a06bae3dd144ecbb
TolaMironcenko <mironcenkotola@gmail.com>
2024-07-02 12:12:05 +0700

    added dwm install script

---
+++ dwm/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# ---- colors ----
+RESET='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+# ----------------
+
+# -------- dwm ---------
+printf "$GREEN installing dwm...$RESET"
+cd ~/dotfiles/dwm/dwm
+make &> /dev/null
+sudo make install &> /dev/null
+if [ $? -eq 0 ]; then
+    printf "[$GREEN ok $RESET]\n"
+else
+    printf "[$RED err $RESET]\n"
+fi
+# ----------------------
+
+# ------- dwmbar -------
+printf "$GREEN installing dwmbar...$RESET"
+cd ~/dotfiles/dwm/dwmbar
+sudo ./install.sh &> /dev/null
+if [ $? -eq 0 ]; then
+    printf "[$GREEN ok $RESET]\n"
+else
+    printf "[$RED err $RESET]\n"
+fi
+# ----------------------
+
