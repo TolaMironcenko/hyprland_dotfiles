6c0a1de1f26abe328b45f9dcb4f17f1ca0597567
TolaMironcenko <mironcenkotola@gmail.com>
2024-07-02 12:29:40 +0700

    added dwm keybinds

--- dwm/dwm/config.def.h
+++ dwm/dwm/config.def.h
@@ -4,15 +4,15 @@
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Nerd:size=10" };
-static const char dmenufont[]       = "JetBrains Mono Nerd:size=10";
+static const char *fonts[]          = { "JetBrains Mono Nerd font:size=12" };
+static const char dmenufont[]       = "JetBrains Mono Nerd font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -63,12 +63,24 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *roficmd[] = { "/home/tola/dotfiles/rofi/launcher/launcher.sh" };
+static const char *roficmd[] = { "/home/tola/dotfiles/rofi/launcher/launcher.sh", NULL };
+static const char *powermenucmd[] = { "/home/tola/dotfiles/rofi/powermenu/powermenu.sh", NULL };
+static const char *clipboardcmd[] = { "/home/tola/dotfiles/rofi/clipboard/clipboard.sh", NULL };
+static const char *rofimojicmd[] = { "/home/tola/dotfiles/rofi/emoji/rofimoji.sh", NULL };
+static const char *filemanagercmd[] = { "thunar", NULL };
+static const char *telegramcmd[] = { "telegram-desktop", NULL };
+static const char *googlecmd[] = { "google-chrome-stable", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
+    { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = powermenucmd } },
+    { MODKEY,                       XK_c,      spawn,          {.v = clipboardcmd } },
+    { MODKEY,                       XK_e,      spawn,          {.v = rofimojicmd} },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_s,      spawn,          {.v = filemanagercmd } },
+    { MODKEY,                       XK_a,      spawn,          {.v = telegramcmd } },
+    { MODKEY,                       XK_g,      spawn,          {.v = googlecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -85,7 +97,7 @@ static const Key keys[] = {
     { MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
     { MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_v,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
