52797cc7afa6aff2e96d36e808c8f2a339724b40
TolaMironcenko <mironcenkotola@gmail.com>
2024-07-10 21:36:55 +0700

    added lisascreens and lisascreenslayout.sh scripts

---
+++ bin/lisascreenslayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output HDMI-0 --mode 1680x1050 --pos 0x0 --rotate normal --output DVI-0 --mode 1920x1200_60.00 --pos 1680x0 --rotate normal --output DVI-1 --off
