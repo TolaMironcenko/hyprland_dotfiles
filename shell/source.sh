f4d6979421e76524760026a50cef931da14e5f25
TolaMironcenko <mironcenkotola@gmail.com>
2024-08-27 20:05:44 +0700

    git aliasses added

--- shell/source.sh
+++ shell/source.sh
@@ -1,3 +1,32 @@
 
 export PATH=$PATH:~/dotfiles/bin:~/dotfiles/hypr/bin:~/dotfiles/dwm/bin
 
+alias g='git'
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gba='git branch --all'
+alias gbd='git branch --delete'
+alias gbD='git branch --delete --force'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gc='git clone'
+alias gcl='git clone --recursive'
+alias gcam='git commit --all --message'
+alias gd='git diff'
+alias gcf='git config --list'
+alias gf='git fetch'
+alias ghh='git help'
+
+alias glgg='git log --graph'
+alias glg='git log --stat'
+
+alias gm='git merge'
+alias gl='git pull'
+alias gpr='git pull --rebase'
+alias gp='git push'
+alias gpo='git push --set-upstream origin'
+alias gra='git remote add'
+alias grrm='git remote remove'
+alias gstall='git stash --all'
+alias gst='git status'
