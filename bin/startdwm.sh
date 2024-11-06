89e0fdb80b173bf8fb3d00b0af325d19b1a8a33f
TolaMironcenko <mironcenkotola@gmail.com>
2024-11-06 20:02:48 +0700

    update startdwm.sh

--- bin/startdwm.sh
+++ bin/startdwm.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export GTK_THEME=Adwaita:dark
+export GTK2_RC_FILES=/usr/share/themes/Adwaita-dark/gtk-2.0/gtkrc
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
 ~/dotfiles/bin/background.sh &
 setxkbmap "us,ru" -option 'grp:win_space_toggle'
 dunst &
