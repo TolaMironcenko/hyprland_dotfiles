64a69b73a896412381e98455ba8aab1f4a981c95
TolaMironcenko <mironcenkotola@gmail.com>
2024-11-23 00:06:54 +0700

    updated hyprlock-background.sh script

--- hypr/bin/hyprlock-background.sh
+++ hypr/bin/hyprlock-background.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 background=$(ls ~/Pictures/wallpaper/*.png | shuf -n 1)
-sed -i "s|.*Pictures.*|\tpath = $background|" ../hyprlock.conf
-sleep 1
+sed -i "s|.*Pictures.*|\tpath = $background|" ~/dotfiles/hypr/hyprlock.conf
 hyprlock
